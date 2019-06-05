c38c62a16f6384aab502fa8ca56dbf2e40733358
rubika25 <36493362+rubika25@users.noreply.github.com>
2019-06-05 10:40:24 +0530

    DB Connection Established

---
+++ Middleware/SwipeIO_Web_API/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwipeIO_Web_API
+{
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server = localhost; port = 3306; database = stud; uid = root; password = ");
+        }
+    }
+
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+
+}
