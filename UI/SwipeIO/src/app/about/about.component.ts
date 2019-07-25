7f3a6d2d78e71adce52f1035294b4f439f3cbf45
Sathish Kumar J <sathishmitnick@gmail.com>
2019-07-25 19:58:53 +0530

    Footer and About page modified

--- UI/SwipeIO/src/app/about/about.component.ts
+++ UI/SwipeIO/src/app/about/about.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+	selector: "app-about",
+	templateUrl: "./about.component.html",
+	styleUrls: ["./about.component.scss"]
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor(private location: Location) {}
+	cancel() {
+		this.location.back();
+	}
+	ngOnInit() {}
 }
--- UI/SwipeIO/src/app/components/components.module.ts
+++ UI/SwipeIO/src/app/components/components.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-
+import { MatButtonModule } from "@angular/material";
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule, RouterModule, MatButtonModule],
 	declarations: [FooterComponent, NavbarComponent, SidebarComponent],
 	exports: [FooterComponent, NavbarComponent, SidebarComponent]
 })
